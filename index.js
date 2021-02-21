4351a27fd6679fcaa63d269cbc808594e8fddad4
sky <54285485+SKY7878@users.noreply.github.com>
2021-02-21 21:59:17 +0400

    added portfolio part and buy me coffee, hire me part,and fixed navbar scroll

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(document).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
+
+
